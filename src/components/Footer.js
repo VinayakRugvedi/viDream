fef0a9b5cbbffe8c1998fadeb284fd3c8c4daba5
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-03-13 15:47:52 +0530

    Create a simple footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '../styles/Footer.scss'
+import footerLogo from '../assets/footerLogo.svg'
+
+function Footer () {
+  return (
+    <div className="footer">
+      <div className="footerLogoContainer">
+        <img src={footerLogo} alt="viDream" className="footerLogo"/>
+      </div>
+      <h1>With <span className="heart">{'\u2764'}</span></h1>
+      <h1>&copy;A Vinayak Rugvedi @Geekskool</h1>
+    </div>
+  )
+}
+
+export default Footer
