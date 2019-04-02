92e42bdfa2e8c1f12256c18115bf4b28ebb808a3
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-04-02 17:12:09 +0530

    Add containers to show video glimpse and other content

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,14 +2,30 @@ import React from 'react'
 
 import '../styles/VideoDetail.scss'
 import closeIcon from '../assets/removeIcon.svg'
+import thumbnail from '../assets/bbcThumbnail.png'
+
+import ReactPlayer from 'react-player'
 
 class VideoDetail extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-
+      toPlay: false
     }
     this.closeVideoDetail = this.closeVideoDetail.bind(this)
+    this.playVideoGlimpse = this.playVideoGlimpse.bind(this)
+  }
+
+  playVideoGlimpse () {
+    this.setState({
+      toPlay: true
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          toPlay: false
+        })
+      }, 10000)
+    })
   }
 
   closeVideoDetail () {
@@ -19,6 +35,30 @@ class VideoDetail extends React.Component {
   render () {
     return (
       <div className="videoDetailHolder">
+          <div className="videoDetailTextHolder">
+            <div className="videoTitleHolder">
+[master fe51332] Add basic styles to Video Detail component to start with?
+            </div>
+            <div className="videoDetailButtons">
+              <button className="videoDetailPlayButton">PLAY</button>
+              <button className="videoDetailAddToListButton">My List    </button>
+            </div>
+          </div>
+          <div className="videoGlimpseInVideoDetail">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=MlRlgbrAVOs"
+              width="100%"
+              height="128%"
+              playing={true}
+              className="videoGlimpsePlayer"
+              onStart={this.playVideoGlimpse}/>
+
+            <div className="videoGlimpseModal">
+              <img src={thumbnail} alt="THUMBNAIL"
+                className="videoGlimpseThumbnail"
+                style={{display: this.state.toPlay ? 'none' : true}}/>
+            </div>
+          </div>
         <div className="closeVideoDetail" onClick={this.closeVideoDetail}>
           <img src={closeIcon} alt="CLOSE" className="closeIcon"/>
         </div>
