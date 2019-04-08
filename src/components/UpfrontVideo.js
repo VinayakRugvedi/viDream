2f61e0739fbc2276d4287e4f1001440a8312e526
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2019-04-09 00:27:27 +0530

    Update, use valid thumbnail

--- src/components/UpfrontVideo.js
+++ src/components/UpfrontVideo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import '../styles/UpfrontVideo.scss'
 import bbcThumbnail from '../assets/bbcThumbnail.png'
-import bbc from '../assets/bbc.mp4'
+// import bbc from '../assets/bbc.mp4'
 import addIcon from '../assets/addIcon.svg'
 import playIcon from '../assets/playIcon.svg'
 import speakerMutedIcon from '../assets/speakerMuted.svg'
@@ -14,7 +14,9 @@ class UpfrontVideo extends React.Component {
   constructor () {
     super ()
     this.state = {
-      playerLoaded: false,
+      apiURL: 'https://www.googleapis.com/youtube/v3/videos',
+      upfrontVideo: {},
+      mediaLoaded: false,
       muted: true,
       playerConfig: {
         youtube: {
@@ -22,7 +24,7 @@ class UpfrontVideo extends React.Component {
             iv_load_policy: 3,
             rel: 0,
             start: 25,
-            end: 48,
+            end: 46,
             controls: 0,
             disablekb: 1,
             autoplay: true
@@ -30,25 +32,25 @@ class UpfrontVideo extends React.Component {
         }
       }
     }
-    this.playerLoaded = this.playerLoaded.bind(this)
+    this.mediaLoaded = this.mediaLoaded.bind(this)
     this.backToThumbnail = this.backToThumbnail.bind(this)
     this.toggleMute = this.toggleMute.bind(this)
   }
 
-  playerLoaded () {
-    setTimeout(() => {
+  mediaLoaded () {
+    // setTimeout(() => {
       this.setState({
-        playerLoaded: true
+        mediaLoaded: true
       }, this.backToThumbnail())
-    }, 2000)
+    // }, 2000)
   }
 
   backToThumbnail () {
     setTimeout(() => {
       this.setState({
-        playerLoaded: false
+        mediaLoaded: false
       })
-    }, 20800)
+    }, 20000)
   }
 
   toggleMute () {
@@ -58,29 +60,35 @@ class UpfrontVideo extends React.Component {
   }
 
   render () {
+    let validThumbnail = this.props.upfrontVideo.videoThumbnails.maxres === undefined ?
+                          this.props.upfrontVideo.videoThumbnails.medium.url :
+                          this.props.upfrontVideo.videoThumbnails.maxres.url
+    console.log(this.props.upfrontVideo.videoId);
     return (
       <div className="upfrontVideoContainer">
         {
           <ReactPlayer
-            url={`https://www.youtube.com/watch?v=c8aFcHFu8QM`}
-            volume={0.5}
+            url={`https://www.youtube.com/watch?v=${this.props.upfrontVideo.videoId}`}
+            volume={1}
             muted={this.state.muted}
             playing={true}
-            width='100%'
+            width="100%"
             className="upfrontVideo"
             config={this.state.playerConfig}
-            onReady={this.playerLoaded}/>
+            onStart={this.mediaLoaded}/>
         }
         <div className="upfrontVideoModal">
-          <img src={bbcThumbnail} alt="THUMBNAIL" className="upfrontThumbnail" style={{display: this.state.playerLoaded ? 'none' : true}}/>
+          <img src={validThumbnail} alt="THUMBNAIL" className="upfrontThumbnail" style={{display: this.state.mediaLoaded ? 'none' : true}}/>
         </div>
         <div className="upfrontVideoDataModal">
           <div className="upfrontVideoDataHolder">
-            <div className="upfrontVideoCategory">
-              Category of the videooo
-            </div>
+            {
+              // <div className="upfrontVideoCategory">
+              //   Category of the videooo
+              // </div>
+            }
             <div className="upfrontVideoTitle">
-              Some random video Title
+              {this.props.upfrontVideo.videoTitle}
             </div>
           </div>
           <div className="upfrontVideoButtons">
@@ -113,12 +121,5 @@ class UpfrontVideo extends React.Component {
     )
   }
 }
-
-// <iframe
-//   className="upfrontVideo"
-//   src="https://www.youtube.com/embed/c8aFcHFu8QM?controls=0&autoplay=true&disablekb=1&fs=0&iv_load_policy=3&modestbranding=1&rel=0&start=25&end=48"
-//   onLoad={this.playerLoaded}
-//   frameBorder="0">
-// </iframe>
 
 export default UpfrontVideo
